e45bbbe4ff91e3b80825208b9296e60e4a8ac2fe
fullstackdevs14 <23446619+fullstackdevs14@users.noreply.github.com>
2023-11-04 12:54:52 -0700

    Implement chatting

--- src/socket.ts
+++ src/socket.ts
@@ -7,7 +7,16 @@ export function setupSocketIO(server: HttpServer) {
   io = new Server(server);
 
   io.on("connection", (socket) => {
-    console.log('Client connected', socket.id);
+    console.log("Client connected", socket.id);
+
+    socket.on("join", (msg) => {
+      socket.join(msg);
+      socket.emit("joined", msg);
+    });
+
+    socket.on("broadcast", (msg) => {
+      socket.broadcast.emit("receive", msg);
+    });
   });
 }
 
